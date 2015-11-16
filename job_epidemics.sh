5071d7eb38fbc1e007d28ac78b5fe9b1a6b253df
Julian Kates-Harbeck <juliankh@rclogin03.rc.fas.harvard.edu>
2015-11-16 03:50:40 -0500

    added slurm script

---
+++ job_epidemics.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH -p general
+#SBATCH -n 64
+#SBATCH -t 10
+#SBATCH --mem-per-cpu 4000
+#SBATCH -o log.out
+#SBATCH -e log.err
+
+cd ~/physics/research/desai/epidemics/src
+julia run_epidemics.jl
