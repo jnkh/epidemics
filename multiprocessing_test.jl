e74d8ad24535ff9a9bfc26b6b1d55d43bee05e70
Julian Kates-Harbeck <juliankh@stanford.edu>
2015-11-22 16:32:23 -0500

    added iteration over N and numprocs

--- multiprocessing_test.jl
+++ multiprocessing_test.jl
@@ -1,25 +1,35 @@
 using SlurmNodes
 
-nl = get_partial_list_of_nodes(200)
-addprocs(nl)
-
-@everywhere N = 400
-Nlist = repmat([N],200)
-@everywhere myfun(N,M) = sum(randn(N,M)^2)
-@everywhere M = N #doens't work without @everywhere!
+N_range = [100,200,400]
+nprocs_range = [8,64,200]
 
 
-tic()
-map(N -> myfun(N,M),Nlist)
-elapsed_serial = toc()
+for N in N_range
+	tic()
+	map(N -> myfun(N,M),Nlist)
+	elapsed_serial = toc()
+	for nprocs in nprocs_range 
+		rmprocs(procs()[2:end])
+		nl = get_partial_list_of_nodes(nprocs)
+		addprocs(nl)
 
-pmap(N -> myfun(N,M),Nlist)
-tic()
-pmap(N -> myfun(N,M),Nlist)
-elapsed_parallel = toc()
+		@everywhere N = 400
+		Nlist = repmat([N],200)
+		@everywhere myfun(N,M) = sum(randn(N,M)^2)
+		@everywhere M = N #doens't work without @everywhere!
 
 
-println("serial: $elapsed_serial, parallel: $elapsed_parallel, speedup: $(elapsed_serial/elapsed_parallel)")
+
+		pmap(N -> myfun(N,M),Nlist)
+		tic()
+		pmap(N -> myfun(N,M),Nlist)
+		elapsed_parallel = toc()
+
+
+		println("N: $N, nprocs: $(nprocs())")
+		println("serial: $elapsed_serial, parallel: $elapsed_parallel, speedup: $(elapsed_serial/elapsed_parallel)")
+	end
+end
 
 
 
