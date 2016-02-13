c4371396822baec5bfbd8146e5684c16253f84de
Julian Kates-Harbeck <julian@Julians-MacBook-Air.local>
2016-02-12 19:06:10 -0500

    rename

--- TwoLevelGraphs.jl
+++ TwoLevelGraphs.jl
@@ -110,7 +110,8 @@ function get_edges_for_supergraph(clusters::Array{Array{Int,1},1},num_edges::Int
     possible_edges = []
     for clust in clusters
         for v in clust
-            for w in get_out_nodes(clusters,v)
+            out_nodes = get_out_nodes(clusters,v)
+            for w in out_nodes
                 if w > v
                     push!(possible_edges,Pair(v,w))
                 end
