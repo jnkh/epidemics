ea8de44ea17043ae489a9aed054cfa47dfb1d0e8
Julian Kates-Harbeck <juliankh91@gmail.com>
2016-09-15 00:28:20 -0400

    using python hostlist

--- SlurmNodes.jl
+++ SlurmNodes.jl
@@ -1,5 +1,8 @@
 module SlurmNodes
 
+using PyCall
+@pyimport hostlist
+
 export get_list_of_nodes,get_partial_list_of_nodes
 
 #returns a zipped list of tuples of the form (nodename<:AbstractString,num_procs_on_node::Int). This can be fed directly to addprocs like addprocs(get_list_of_nodes()).
@@ -33,17 +36,18 @@ function get_list_of_nodes()
 	println("nodelist: $(ENV["SLURM_NODELIST"])")
 	println("cpus_per_node: $(ENV["SLURM_JOB_CPUS_PER_NODE"])")
 	s = ENV["SLURM_NODELIST"]
+    node_list = hostlist.expand_hostlist(s)
 	cpus_per_node = reduce(vcat,map(convert_cpus_strings_to_ints,split(ENV["SLURM_JOB_CPUS_PER_NODE"],',')))
-	idx = searchindex(s,"[")
-	if idx > 0
-		num_digits = length(split(split(s[idx+1:end-1],",")[end],"-")[end])
-		numlist = split(s[idx+1:end-1],",")
-		node_numbers = generate_list_of_node_numbers(numlist)
-		node_str = s[1:idx-1]
-		node_list = [node_str*pad_zeros(n,num_digits) for n in node_numbers]
-	else
-		node_list = [s]
-	end
+	# idx = searchindex(s,"[")
+	# if idx > 0
+	# 	num_digits = length(split(split(s[idx+1:end-1],",")[end],"-")[end])
+	# 	numlist = split(s[idx+1:end-1],",")
+	# 	node_numbers = generate_list_of_node_numbers(numlist)
+	# 	node_str = s[1:idx-1]
+	# 	node_list = [node_str*pad_zeros(n,num_digits) for n in node_numbers]
+	# else
+	# 	node_list = [s]
+	# end
 	return collect(zip(node_list,cpus_per_node))
 end
 
