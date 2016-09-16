8314d449f5a8bfa59eebd11a4ada386712306904
Julian Kates-Harbeck <juliankh91@gmail.com>
2016-09-15 20:31:23 -0400

    trying to get parallel execution to work

--- run_epidemics.jl
+++ run_epidemics.jl
@@ -1,7 +1,7 @@
 function save_epidemics_results(params)
 	data_dir_path = "../data/"  #"/mnt/D/windows/MIT/classes/6/338/project/data/"
 
-@everywhere begin
+	# @everywhere begin
 	N::Int = params["N"]
 	alpha::Float64 = params["alpha"]
 	beta::Float64 = params["beta"]
@@ -12,7 +12,7 @@ function save_epidemics_results(params)
 	num_trials = params["num_trials"]
 	num_trials_mixed = params["num_trials_mixed"]
 	graph_model = params["graph_model"]
-  graph_information = params["graph_information"]
+  	graph_information = params["graph_information"]
 
 	#function f1(x::Float64) 1 + alpha::Float64*x end
 	#function f2(x::Float64)  1 + beta::Float64 end
@@ -28,39 +28,40 @@ function save_epidemics_results(params)
 
 	y_n, y_minus, y_plus, y_p,critical_determinant = get_parameters(N,alpha,beta,verbose)
 
-	end
+	# end
 
-
-	println("running in parallel on $(nprocs()-1) nodes...")
-	tic()
-	if graph_model
-	@time runs =
-        run_epidemics_parallel(num_trials, () -> run_epidemic_graph(N,im_normal,graph_information,fixation_threshold),in_parallel);
-	else
-		if k < N-1
-			@time runs =
-			run_epidemics_parallel(num_trials_mixed, () -> run_epidemic_well_mixed(N,im_effective,fixation_threshold),in_parallel);
+	if myid() == 1:
+		println("running in parallel on $(nprocs()-1) nodes...")
+		tic()
+		if graph_model
+		@time runs =
+	        run_epidemics_parallel(num_trials, () -> run_epidemic_graph(N,im_normal,graph_information,fixation_threshold),in_parallel);
 		else
-			@time runs =
-			run_epidemics_parallel(num_trials_mixed, () -> run_epidemic_well_mixed(N,im_normal,fixation_threshold),in_parallel);
+			if k < N-1
+				@time runs =
+				run_epidemics_parallel(num_trials_mixed, () -> run_epidemic_well_mixed(N,im_effective,fixation_threshold),in_parallel);
+			else
+				@time runs =
+				run_epidemics_parallel(num_trials_mixed, () -> run_epidemic_well_mixed(N,im_normal,fixation_threshold),in_parallel);
+			end
 		end
+		elapsed = toc()
+		println("done after $elapsed seconds.")
+
+
+		structure = graph_model ? "graph" : "mixed"
+		filename = "epidemics_$(structure)_$(now()).jld"
+		save(data_dir_path * filename,"params",params,"runs",runs)
+
+
+		timing_filename = "timing_$(structure)_$(now()).jld"
+		save(data_dir_path * timing_filename,"params",params,"elapsed",elapsed)
+
+		timing_log_filename = "timing_log.out"
+		f = open(timing_log_filename,"a")
+		nt = graph_model ? num_trials : num_trials_mixed
+		write(f,"$N $k $nt $elapsed $graph_model $(now())\n")
 	end
-	elapsed = toc()
-	println("done after $elapsed seconds.")
-
-
-	structure = graph_model ? "graph" : "mixed"
-	filename = "epidemics_$(structure)_$(now()).jld"
-	save(data_dir_path * filename,"params",params,"runs",runs)
-
-
-	timing_filename = "timing_$(structure)_$(now()).jld"
-	save(data_dir_path * timing_filename,"params",params,"elapsed",elapsed)
-
-	timing_log_filename = "timing_log.out"
-	f = open(timing_log_filename,"a")
-	nt = graph_model ? num_trials : num_trials_mixed
-	write(f,"$N $k $nt $elapsed $graph_model $(now())\n")
 
 end
 
@@ -152,9 +153,7 @@ end
 	    #     remotecall_fetch(p,(x,y,z) -> (params["k"] = x; params["graph_model"] = y;params["graph_type"] = z),k,graph_model,graph_type)
 	    # end
 
-	    if myid() == 1
-	    	save_epidemics_results(params)
-	    end
+	    save_epidemics_results(params)
 
 	end
 	end
