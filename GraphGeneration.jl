181a9f0025e8b2028a8a2993faf90b230e636693
Julian Kates-Harbeck <juliankh91@gmail.com>
2016-11-01 02:15:12 -0400

    minimum degree

--- GraphGeneration.jl
+++ GraphGeneration.jl
@@ -141,7 +141,7 @@ function sample_integers(d::UnivariateDistribution,N::Int,min_degree=1)
 end
 
 function sample_degrees(d::UnivariateDistribution,N::Int,min_degree=1)
-    degrees = sample_integers(d,N)
+    degrees = sample_integers(d,N,min_degree)
     #make sure sum is even
     while sum(degrees) % 2 != 0
         degrees = sample_integers(d,N)
