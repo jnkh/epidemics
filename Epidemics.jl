656fccfed7b4d641b49b6016c73246ab63e332b0
Julian Kates-Harbeck <julian@Julians-MacBook-Pro.local>
2017-02-02 21:00:37 -0500

    adding raw graph to graph information

--- Epidemics.jl
+++ Epidemics.jl
@@ -196,6 +196,7 @@ function run_epidemic_graph(N::Int,im::InfectionModel,graph_information::GraphIn
     frac = get_fraction_of_type(p,INFECTED)
     push!(infecteds,N*frac)
     if carry_by_node_info
+        graph_information.graph = g
         push!(infecteds_by_nodes,copy(get_payload(p)))
     end
 
