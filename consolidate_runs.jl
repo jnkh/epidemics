d2a4346424e12fbcff856ab4e1dd489c3528d7c5
Julian Kates-Harbeck <julian@Julians-MacBook-Pro.local>
2017-02-08 00:05:20 -0500

    fixed path

--- consolidate_runs.jl
+++ consolidate_runs.jl
@@ -4,7 +4,7 @@ path = "/n/regal/desai_lab/juliankh/tmp/"
 outpath = "~/physics/research/desai/epidemics/data/"
 dirs = split(readall(`ls $path`));
 for (i,curr_dir) in enumerate(dirs)
-    consolidate_epidemic_runs(path * curr_dir * "/data",outpath)
+    consolidate_epidemic_runs(path * curr_dir * "/data/",outpath)
     run(`rm -rf $(path)/$(curr_dir)`)
     println("$i/$(length(dirs))")
 end
