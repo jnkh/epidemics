4eadd368aa2d384f1a1dd2dc5a27d2a3312ac091
Julian Kates-Harbeck <juliankh@rclogin02.rc.fas.harvard.edu>
2017-10-26 01:05:56 -0400

    removed plotting code

--- generate_p_reach_figures.jl
+++ generate_p_reach_figures.jl
@@ -1,27 +1,30 @@
-addprocs(4)
+using SlurmNodes
+addprocs(get_list_of_nodes())
 push!(LOAD_PATH, pwd())
-using SIS,IM,PayloadGraph,PyPlot, Epidemics,JLD,
+using PyCall
+#using PyPlot,Plotting
+#pygui(:qt)
+using SIS,IM,PayloadGraph, Epidemics,JLD,
 Dierckx,Plotting,DataAnalysis,GraphGeneration,
 TwoDegree,DegreeDistribution,GraphClustering,TwoLevelGraphs,
-Plotting
 import LightGraphs
 
-save_path = "../data/plot.jld"
+save_path = "../data/figure_data_regular.jld"
 
 plt[:rc]("text",usetex=true)
 
 N = 400
-k = 100
+k = 10
 alpha=0.5
 beta = 0.05
 num_trials_th = 1000
 num_trials_sim = 1000
 graph_type = regular_rg
-num_trials_sim_range = [1000,1000,1000,1000]
-k_range = [4,10,40]#,399]
+num_trials_sim_range = 1000*ones(Int,4) #[1000,1000,1000,100]
+k_range = [4,10,40,399]
 color_range = ["b","r","g","k"]
 labels = [L"k = $k" for k in k_range]
-in_parallel= false
+in_parallel= true
 
 results = SimulationResult[]
 if in_parallel
@@ -34,16 +37,17 @@ for (i,k) in enumerate(k_range)
     @time si = get_simulation_result(N,alpha,beta,gi,num_trials_th,num_trials_sim_range[i],in_parallel=in_parallel)
     push!(results,si)
 end
+rmprocs(procs())
 prepare_for_saving.(results)
 save(save_path,"results",results)
 d = load(save_path)
 results = d["results"]
 
-figure(dpi=200)
-color_range = ["b","r","g","k"]
-labels = [latexstring("k = $k") for k in k_range]
-for (i,si) in enumerate(results)
-    plot_simulation_result(si,color=color_range[i],label=labels[i],num_points=12+i)
-end
-legend(fontsize=18,frameon=false,loc="lower left")
-savefig("test.png",bbox_to_anchor="full")
+#figure(dpi=300)
+#color_range = ["b","r","g","k"]
+#labels = [latexstring("k = $k") for k in k_range]
+#for (i,si) in enumerate(results)
+#    plot_simulation_result(si,color=color_range[i],label=labels[i],num_points=12+i)
+#end
+#legend(fontsize=18,frameon=false,loc="lower left")
+#savefig("test.png",bbox_to_anchor="full")
