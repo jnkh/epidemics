cbd1007b726a6b82bb7a22c6a5829017c2400742
juliankh <juliankh@boslogin01.rc.fas.harvard.edu>
2019-06-24 21:57:58 -0400

    swap names

--- figure_generation/fig4/fig4b.jl
+++ figure_generation/fig4/fig4b.jl
@@ -12,11 +12,11 @@ else
 	println("Running in serial.")
 end
 @everywhere using JLD2,FileIO,Random, Munkres, Distributions,StatsBase,NLsolve,LightGraphs,SimpleGraphs,Dierckx,PyPlot,Plots,PyCall,Dates,Interpolations
-# @everywhere import_path_desai = "/n/home07/juliankh/physics/research/desai/epidemics/src"
-@everywhere import_path_desai = "../.././"
+@everywhere import_path_desai = "/n/home07/juliankh/physics/research/desai/epidemics/src"
+#@everywhere import_path_desai = "../.././"
 @everywhere push!(LOAD_PATH, import_path_desai)
-# @everywhere import_path_nowak = "/n/home07/juliankh/physics/research/nowak/indirect_rec/src"
-@everywhere import_path_nowak = "/Users/julian/Harvard/research/nowak/indirect_rec/src/"
+@everywhere import_path_nowak = "/n/home07/juliankh/physics/research/nowak/indirect_rec/src"
+#@everywhere import_path_nowak = "/Users/julian/Harvard/research/nowak/indirect_rec/src/"
 @everywhere push!(LOAD_PATH, import_path_nowak)
 @everywhere using Epidemics,IM, GraphGeneration,DegreeDistribution,GraphGeneration,GraphCreation,GraphClustering,TwoLevelGraphs,DataAnalysis,GraphPlotting,Plotting
 
@@ -48,61 +48,72 @@ rcParams["lines.linewidth"] = 1.5
 rcParams["font.size"] = 20
 
 ###Functions###
-save_path = "../data/fig4b.jld2"
+save_path = "../data/fig4c.jld2"
 generate_data = true
 plot_beta = false
 
 if generate_data
-   plot_beta = false
-    N = 50000;k=10;sigma_k = 0 #N = 20000
-    N_small = 2000
-    # N_range = [120,200,320,440]#,800,1600,3200]
-    # alpha_range = [0.05,0.15,0.18,0.2,0.22,0.25,0.28,0.5,1.0]#linspace(0.01,1.0,5)#8*[0.005,0.01,0.02,0.04,0.06,0.1,0.15,0.25]#,0.3]#collect(0.01:0.1:0.5)
-    alpha_range = 10.0.^range(log10(0.01),stop=log10(1.0),length=40)#8*[0.005,0.01,0.02,0.04,0.06,0.1,0.15,0.25]#,0.3]#collect(0.01:0.1:0.5)
-    # k_range = [4,6,8,10,15,20]
-    # alpha_range = logspace(log10(0.01),log10(2.0),10)#8*[0.005,0.01,0.02,0.04,0.06,0.1,0.15,0.25]#,0.3]#collect(0.01:0.1:0.5)
+N = 50000;k=20;m=20;l=19;r = k-l #N = 100000
+N_small = 2000 #4000
+alpha_range = 10.0.^range(log10(0.01),stop=log10(1.5),length=30)#8*[0.005,0.01,0.02,0.04,0.06,0.1,0.15,0.25]#,0.3]#collect(0.01:0.1:0.5)
 
-    beta = 0.025
-    sigma_k_arr = [0,5,10,15,20,25,30]#[0,1,5,15,30]#collect(4:40)#[4,6,8,10,15,20,30,40]#k
-    l2 = length(sigma_k_arr)
-    l1 = length(alpha_range)
-    # l2 = length(N_range)
-    phase_trans_arr = zeros(Float64,l1,l2)
-    phase_trans_simple_arr = zeros(Float64,l1,l2)
-    pfix_arr = zeros(Float64,l1,l2)
-    pfix_arr_small = zeros(Float64,l1,l2)
-    comm_ratio_arr = zeros(Float64,l1,l2)
-    # beta = 0.05
-    num_trials = 400
-    num_trials_th = 1000
-    num_trials_sim = 400_000
-    pregenerate_graph = true
-    for (i,alpha) in enumerate(alpha_range)
-        for (j,sigma_k) in enumerate(sigma_k_arr)
-            graph_type = sigma_k == 0 ? regular_rg : gamma_rg
-            gi = get_graph_information(graph_type,N=N,k=k,sigma_k=sigma_k,pregenerate_graph=pregenerate_graph)
-            @time sr = get_simulation_result(N,alpha,beta,gi,num_trials_th,num_trials_sim,in_parallel=in_parallel) 
-            pfix_arr[i,j] = get_pfix_sim(sr)[1]
-
-            phase_trans_simple_arr[i,j] = sparsity_cascade_condition_simple(alpha,beta,k)
-            phase_trans_arr[i,j] = sparsity_cascade_condition(N,alpha,beta,k)
-
-            gi = get_graph_information(graph_type,N=N_small,k=k,sigma_k=sigma_k,pregenerate_graph=pregenerate_graph)
-            @time sr = get_simulation_result(N_small,alpha,beta,gi,num_trials_th,num_trials_sim,in_parallel=in_parallel) 
-            pfix_arr_small[i,j] = get_pfix_sim(sr)[1]
-
-        end
-        println(".")
+beta = 0.025
+#     l_range = [1,2,4,6,8,10,12,14,16,17,18,19] 
+l_range = [1,4,8,11,13,14,15,16,17,18,19] 
+l2 = length(l_range)
+l1 = length(alpha_range)
+# l2 = length(N_range)
+phase_trans_arr = zeros(Float64,l1,l2)
+pfix_arr = zeros(Float64,l1,l2)
+pfix_arr_small = zeros(Float64,l1,l2)
+comm_ratio_arr = zeros(Float64,l1,l2)
+# beta = 0.05
+num_trials = 400
+num_trials_sim = 400_000
+num_trials_th = 100
+graph_type = two_level_rg
+pregenerate_graph = true
+for (i,alpha) in enumerate(alpha_range)
+    for (j,l) in enumerate(l_range)
+#     for (j,l) in enumerate(l_range)
+#     for (j,beta) in enumerate(beta_range)
+        r = k-l
+#     for (j,N) in enumerate(N_range)
+        t = get_t(N,k,m,l)
+        gi = get_graph_information(graph_type,N=N,k=k,m=m,l=l,r=r,pregenerate_graph=pregenerate_graph)
+        @time sr = get_simulation_result(N,alpha,beta,gi,num_trials_th,num_trials_sim,in_parallel=in_parallel) 
+        pfix_arr[i,j] = get_pfix_sim(sr)[1]
+#         _,_,pfix,_ = Epidemics.get_simulation_yy_pp(gi,N,alpha,beta,num_trials_th,num_trials_sim=num_trials,use_theory=true)
+#         pfix_arr[i,j] = pfix
+        phase_trans_arr[i,j] = phase_transition_condition(t,alpha,beta)
+        comm_ratio_arr[i,j] = get_community_graph_fixation_ratio(t,alpha,beta)
+#         N_small = Int(round(N/100))
+        t = get_t(N_small,k,m,l)
+        gi = get_graph_information(graph_type,N=N_small,k=k,m=m,l=l,r=r,pregenerate_graph=pregenerate_graph)
+        @time sr = get_simulation_result(N_small,alpha,beta,gi,num_trials_th,num_trials_sim,in_parallel=in_parallel) 
+        pfix_arr_small[i,j] = get_pfix_sim(sr)[1]
+    print(".")
     end
-	@save save_path pfix_arr pfix_arr_small phase_trans_arr phase_trans_simple_arr num_trials_sim num_trials_th alpha beta N N_small k_range 
+    println()
+end
+	@save save_path pfix_arr pfix_arr_small phase_trans_arr comm_ratio_arr num_trials_sim num_trials_th alpha beta N N_small k_range 
 else
-    @load save_path pfix_arr pfix_arr_small phase_trans_arr phase_trans_simple_arr num_trials_sim num_trials_th alpha beta N N_small k_range 
+    @load save_path pfix_arr pfix_arr_small phase_trans_arr comm_ratio_arr num_trials_sim num_trials_th alpha beta N N_small k_range 
 end
 
 rmprocs(workers())
 
 
-###Draw Figure###
+using Interpolations
+alpha_range_loc = 10.0.^range(log10(minimum(alpha_range)),stop=log10(maximum(alpha_range)),length=100)
+t = get_t(N,k,m,l)
+comm_ratio_loc = [get_community_graph_fixation_ratio(t,alpha,beta) for alpha in alpha_range_loc]
+# get_community_graph_fixation_ratio(t,alpha,beta)
+itp = interpolate((comm_ratio_loc,),alpha_range_loc,Gridded(Linear()))
+alpha_crit_comm_ratio = itp[1.0]
+alpha_crit_k = k*beta
+
+
 figure(dpi=200)
 # axhline(xz,color="k",linestyle ="-")
 # contourf(beta_range,alpha_range,pfix_arr,200,vmin=eps,vmax = 1.,cmap="inferno")#,norm=matplotlib[:colors][:LogNorm](vmin=0.001,vmax=0.1,clip=true))#logspace(0,1,20)[1:end-1]))#) #cmap="inferno"
@@ -113,21 +124,26 @@ tmp = clamp.(pfix_arr ./ (pfix_arr_small .+ 1e-10),vmin,vmax)
 # contourf(l_range,N_range,pfix_arr,200,cmap="inferno")#,norm=matplotlib[:colors][:LogNorm](vmin=0.001,vmax=0.1,clip=true))#logspace(0,1,20)[1:end-1]))#) #cmap="inferno"
 alpha_range_loc = range(minimum(alpha_range),stop=maximum(alpha_range),length=100)
 if plot_beta
+    x_arr = beta_range
     xlabel(L"\beta",size=20)
-#     yn_transition = beta_range'./(alpha_range_loc.*ones(x_arr)') - 1/k
+    yn_transition = beta_range'./(alpha_range_loc.*ones(x_arr)') - 1/k
 else
-    xlabel(L"k",size=20)
-#     yn_transition = beta./(alpha_range_loc.*ones(x_arr)') - 1/k
+    xlabel(L"k_i",size=20)
+    x_arr = l_range
+    yn_transition = beta./(alpha_range_loc.*ones(length(x_arr))') .- 1/k
 end
-contourf(k_range,alpha_range,tmp,500,vmin=vmin,vmax=vmax,cmap="gnuplot")#vmin=vmin,vmax = vmax,cmap="inferno")#,norm=matplotlib[:colors][:LogNorm](vmin=0.001,vmax=0.1,clip=true))#logspace(0,1,20)[1:end-1]))#) #cmap="inferno"
+contourf(x_arr,alpha_range,tmp,500,vmin=vmin,vmax=vmax,cmap="gnuplot")#vmin=vmin,vmax = vmax,cmap="inferno")#,norm=matplotlib[:colors][:LogNorm](vmin=0.001,vmax=0.1,clip=true))#logspace(0,1,20)[1:end-1]))#) #cmap="inferno"
 cb = colorbar(ticks=0:0.1:1)
-contour(k_range,alpha_range,phase_trans_arr,[1.0],linestyles="--",colors="w") #negative selection regime starts
-contour(k_range,alpha_range,phase_trans_simple_arr,[0.0],linestyles="-",colors="w") #above this we have at worst 1/sqrt(N) scaling
+contour(x_arr,alpha_range,phase_trans_arr,[1.0],colors="w") #mild to no scaling transition
+contour(x_arr,alpha_range,comm_ratio_arr,[1.0],linestyles="-",colors="k") #ratio of exactly 1 transition
 # contour(x_arr,alpha_range_loc,yn_transition,[-0.02,0.0,0.02],linestyles="--",colors="w")
-contour(k_range,alpha_range,tmp,[sqrt(N_small/N),1.0],linestyles=":",linewidths=5,colors=["w","k"])#vmin=vmin,vmax = vmax,cmap="inferno")#,norm=matplotlib[:colors][:LogNorm](vmin=0.001,vmax=0.1,clip=true))#logspace(0,1,20)[1:end-1]))#) #cmap="inferno"
+contour(x_arr,alpha_range,tmp,[sqrt(N_small/N),1.0],linestyles=":",linewidths=5,colors=["w","k"])#vmin=vmin,vmax = vmax,cmap="inferno")#,norm=matplotlib[:colors][:LogNorm](vmin=0.001,vmax=0.1,clip=true))#logspace(0,1,20)[1:end-1]))#) #cmap="inferno"
 gca()[:tick_params](labelsize=15)
-# gca()[:set_xticks]([1,5,10,15,19])
+gca()[:set_xticks]([1,5,10,15,19])
 cb[:ax][:tick_params](labelsize=15)
 # gca()[:set_yscale]("log")
 ylabel(L"\alpha",size=20)
-PyPlot.savefig("fig_4_degree_distribution_phase_transition.pdf",bbox_inches="tight",transparent=true)
+annotate("", xy=(1, alpha_crit_k), xytext=(-0.5, alpha_crit_k),arrowprops=Dict("facecolor"=>"w", "width"=>2.0,"headwidth"=>7.0, "shrink"=>0.04))
+annotate("", xy=(19, alpha_crit_comm_ratio), xytext=(17.5, alpha_crit_comm_ratio),arrowprops=Dict("facecolor"=>"k", "width"=>2.0,"headwidth"=>7.0, "shrink"=>0.04))
+# annotate("", ,arrowprops=Dict("facecolor"=>"k", "shrink"=>0.05))
+PyPlot.savefig("fig_4_community_phase_transition.pdf",bbox_inches="tight",transparent=true)
